acdab7d526f191cd796b34b3e65264d386957995
Андрей <andrews.ru83@gmail.com>
2022-05-11 22:12:00 +0300

    Task.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.time.LocalDate;
+
 public class Main {
+
+    public static void determiningVersion (int os , int year) {
+        int currentYear = LocalDate.now().getYear() ;
+        if (os == 1) {
+            if (year == currentYear) {
+                System.out.println("Установите обычную версию приложения для Android");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android");
+            }
+        } else {
+            if (year >= 2015) {
+                System.out.println("Установите обычную версию приложения для iOS");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS");
+            }
+        }
+
+    }
     public static void main(String[] args) {
+
+        byte clientOS = 0;
+        int currentYear = 2022;
+//        System.out.println(currentYear);
+        determiningVersion(clientOS , currentYear);
 
     }
 }
